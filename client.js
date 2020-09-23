77789f6cf9532d7bf9650c741f6cd8a79bf5c2a2
NKadish <noahkadish@hotmail.com>
2020-09-23 17:04:01 +0000

    Added something that tells you when you connected as well as initials for the snekko.

--- client.js
+++ client.js
@@ -10,8 +10,14 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log('Connected to the server!');
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+  });
+  conn.on('connect', () => {
+    conn.write('Name: NK');
   });
   return conn;
 }
